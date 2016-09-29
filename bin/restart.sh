63c4fbbb97f112af24211cac7d7fcbc0744dcf22
hoswey <hongshuwei@gmail.com>
2016-09-29 12:28:12 +0800

    update restart script

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-cd ..
-ps -ef | grep "cms.report" | awk '{print $2}' | xargs sudo kill -9
+ps -ef | grep cms | grep node | awk '{print $2}' | xargs sudo kill -9
 sudo nohup npm start &
