88524c7a1477953ed89f6bbd9d957457ee444af6
hoswey <hongshuwei@gmail.com>
2016-10-09 16:54:40 +0800

    no message

--- src/app/shared/component/server-filter-tips/server-filter-tips.component.ts
+++ src/app/shared/component/server-filter-tips/server-filter-tips.component.ts
@@ -4,7 +4,7 @@ import {Component} from "@angular/core";
   selector: "server-filter-tips",
   template: `
           <ba-card title="" baCardClass="container with-scroll lists-widget">
-            <div class="accent" style="margin-top: 0px;">Tips:由于该表是大数据量表，为提高效率，数据默认不显示. 请输择［From］&［To］日期, 输入查询条件，再点击[查询] 
+            <div class="accent" style="margin-top: 0px;">Tips:由于该表是大数据量表，为提高效率，数据默认不显示. 请选择［From］&［To］日期, 输入查询条件，再点击[查询] 
             </div>
           </ba-card>
   `
